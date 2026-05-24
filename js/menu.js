50cf6d1e58a7768323180f1975cb08d7104579ea
H. Cumaili <kholioosm@gmail.com>
2026-05-24 04:01:38 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,70 @@
+/* =========================================
+   Menu System
+========================================= */
 
+
+/* =========================
+MENU ELEMENTS
+========================= */
+
+const menuTrigger =
+document.getElementById(
+  "menuTrigger"
+);
+
+const menuOverlay =
+document.getElementById(
+  "menuOverlay"
+);
+
+const menuClose =
+document.getElementById(
+  "menuClose"
+);
+
+
+/* =========================
+MENU
+========================= */
+
+if(
+
+  menuTrigger &&
+
+  menuOverlay &&
+
+  menuClose
+
+){
+
+  menuTrigger
+  .addEventListener(
+
+    "click",
+
+    ()=>{
+
+      menuOverlay
+      .classList
+      .add("active");
+
+    }
+
+  );
+
+  menuClose
+  .addEventListener(
+
+    "click",
+
+    ()=>{
+
+      menuOverlay
+      .classList
+      .remove("active");
+
+    }
+
+  );
+
+}
