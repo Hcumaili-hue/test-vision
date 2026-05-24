899def39bc866287d0a99919b27310c8bd833163
H. Cumaili <kholioosm@gmail.com>
2026-05-24 04:10:42 +0300

    Update tables.js

--- js/tables.js
+++ js/tables.js
@@ -1,1 +1,21 @@
+/* =========================================
+   Tables System
+========================================= */
 
+
+/* =========================
+ACADEMIC TABLES
+========================= */
+
+const comparisonTables =
+document.querySelectorAll(
+  ".comparison-table"
+);
+
+if(comparisonTables.length){
+
+  console.log(
+    "Academic tables loaded"
+  );
+
+}
