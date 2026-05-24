b8e007b937873467bf3dab98af137f47bb14738a
H. Cumaili <kholioosm@gmail.com>
2026-05-24 04:06:34 +0300

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,1 +1,54 @@
+/* =========================================
+   Cards System
+========================================= */
 
+
+/* =========================
+ACADEMIC NETWORK EFFECT
+========================= */
+
+const networkCards =
+document.querySelectorAll(
+  ".network-card"
+);
+
+if(networkCards.length){
+
+  networkCards.forEach(
+
+    card=>{
+
+      card.addEventListener(
+
+        "click",
+
+        function(e){
+
+          e.preventDefault();
+
+          const url =
+
+          this.getAttribute(
+            "href"
+          );
+
+          this.classList.add(
+            "network-open"
+          );
+
+          setTimeout(()=>{
+
+            window.location.href =
+            url;
+
+          },320);
+
+        }
+
+      );
+
+    }
+
+  );
+
+}
